9e7841a24ee1e24392ccf86f6cf8420f8b0e32cf
dvorson <dvorson@gmail.com>
2019-06-03 21:16:09 +0300

    fixed static paths

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const routes = require('./routing');
 
 const app = express();
 
-app.use('/static', express.static('./server/static'));
-app.use('/build', express.static('./build'));
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
+app.use('/build', express.static(path.resolve(__dirname, '../build')));
 routes(app);
 
 // This is fired every time the server-side receives a request.
