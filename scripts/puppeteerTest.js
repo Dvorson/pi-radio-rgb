c0964332fa9b74007bc20998b60b5364b6fb9d76
dvorson <dvorson@gmail.com>
2020-09-11 13:23:13 +0300

    pup test fix

--- scripts/puppeteerTest.js
+++ scripts/puppeteerTest.js
@@ -1,13 +1,12 @@
 const puppeteer = require('puppeteer');
 const Promise = require('bluebird');
+const os = require('os');
 
-const { getLocalStations } = require('./external/radio.net');
+const { getLocalStations } = require('../lib/external/radio.net');
+
+let browser;
 
 async function preload(url) {
-    if (!browser) browser = await puppeteer.launch({
-        ignoreDefaultArgs: ['--mute-audio'],
-        ...(os.platform() === 'linux' && { executablePath: 'chromium-browser', args: ['--no-sandbox'] })
-    });
     const urlPage = await browser.newPage();
     await urlPage.goto(url);
     await urlPage.evaluate(() => document.getElementsByTagName('video')[0].pause());
@@ -15,8 +14,13 @@ async function preload(url) {
 }
 
 (async () => {
+    if (!browser) browser = await puppeteer.launch({
+        ignoreDefaultArgs: ['--mute-audio'],
+        ...(os.platform() === 'linux' && { executablePath: 'chromium-browser', args: ['--no-sandbox'] })
+    });
     const stations = await getLocalStations();
     const streamUrls = stations.map(({ streamUrls }) => streamUrls[0].streamUrl);
     pages = await Promise.reduce(streamUrls, async (acc, url) => ({ ...acc, [url]: await preload(url) }), {});
     console.log({ pages });
-})
+    process.exit();
+})();
